e4e49f6cd5fec317134ed614a15c9b8e616fa313
JoaoCarvalho12 <1231540@isep.ipp.pt>
2025-11-14 19:36:51 +0000

    Terraform plan

--- obj/Debug/net9.0/ProductsAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bac686547bd2e59ff2f45225616dccfbd36d4eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e99ce59022b648c52ecc003401a5b8fe01f100")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
